51dc2843ede611872c549b93e9cbf9ea3dfe5e85
Rishank kumar <mrrishank91@gmail.com>
2026-02-03 23:36:11 +0530

    BMICalc

---
+++ basic program/BMICalc.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BMICalc{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double weight = sc.nextDouble();   // kg
+        double heightCm = sc.nextDouble(); // cm
+
+        double heightM = heightCm/100;
+        double bmi = weight/(heightM * heightM);
+
+        System.out.println("BMI = " + bmi);
+
+        if(bmi < 18.5){
+			System.out.println("Underweight");
+		} else if(bmi < 25){
+			System.out.println("Normal");
+        } else if(bmi < 40){
+			System.out.println("Overweight");
+        } else {
+			System.out.println("Obese");
+    }
+}
+}
