b39efa30d7415193ec15816ed9d01dda48d45881
Rishank kumar <mrrishank91@gmail.com>
2026-02-03 23:38:27 +0530

    Countdown

---
+++ basic program/Countdown.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Countdown{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int n =  sc.nextInt();
+		while (n>=1){
+			System.out.println(n);
+			n--;
+		}
+	}
+}
