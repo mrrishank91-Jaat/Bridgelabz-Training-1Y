0c6f6e28681e317639dd0ffca7a539ff02ce1662
Rishank kumar <mrrishank91@gmail.com>
2026-02-04 21:56:34 +0530

    SquareSide

---
+++ basic program/SquareSide.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class SquareSide{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+			
+		float pm = sc.nextFloat();
+		float side = pm / 4;
+			
+		System.out.println("The length of the side is " +side + "whose perimeter is " + pm);
+	}
+}
