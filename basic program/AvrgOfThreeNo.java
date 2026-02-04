1a3ebc81b668be9d4dbb326f7d8556c9d37f242d
Rishank kumar <mrrishank91@gmail.com>
2026-02-04 22:53:32 +0530

    AvrgOfThreeNo

---
+++ basic program/AvrgOfThreeNo.java
@@ -0,0 +1,9 @@
+public class AvrgOfThreeNo{
+	public static void main(String[] args){
+		float a = 10; 
+		float b = 20;
+		float c = 55;
+		double res = ( a + b + c)/3;
+		System.out.println("Average =" +res);
+	}
+}	
