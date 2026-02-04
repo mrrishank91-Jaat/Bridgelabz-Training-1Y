9c2b953d6575904fa99bee15771867a5a9a65900
Rishank kumar <mrrishank91@gmail.com>
2026-02-04 22:44:07 +0530

    HarshadNUmber

---
+++ basic program/HarshadNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class HarshadNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int original = num;
+        int sum = 0;
+
+        while(num != 0){
+            sum += num % 10;
+            num /= 10;
+        }
+
+        if(original % sum == 0)
+            System.out.println("Harshad Number");
+        else
+            System.out.println("Not a Harshad Number");
+    }
+}
