6b09406d005834b411528e3f9bddbb46a2f7d99a
Rishank kumar <mrrishank91@gmail.com>
2026-02-04 21:45:44 +0530

    VolOfCylinder

---
+++ basic program/VolOfCylinder.java
@@ -0,0 +1,9 @@
+public class VolOfCylinder{
+	public static void main(String[] args){
+		double r = 5;
+		double h = 10;
+		double volume = Math.PI*r*r*h;
+		
+		System.out.println("Volume =" + volume);
+	}
+}
