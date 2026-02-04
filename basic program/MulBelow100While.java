87988524e9e0473fbae0e46b770e71eab74e35a5
Rishank kumar <mrrishank91@gmail.com>
2026-02-04 22:30:18 +0530

    MulBelow100While

---
+++ basic program/MulBelow100While.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MulBelow100While{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        if (number>0 && number<100){
+            int counter = number - 1;
+            while (counter > 1){
+                if (number % counter == 0){
+                    System.out.println(counter);
+                }
+                counter--;
+            }
+        } else {
+            System.out.println("Enter a positive number less than 100");
+        }
+    }
+}
+         
