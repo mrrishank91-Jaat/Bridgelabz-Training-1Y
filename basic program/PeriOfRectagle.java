a2a1dae8714ed608ca96390447dbe75d19adce8d
Rishank kumar <mrrishank91@gmail.com>
2026-02-04 22:14:50 +0530

    PeriOfRectagle

---
+++ basic program/PeriOfRectagle.java
@@ -0,0 +1,9 @@
+public class PeriOfRectagle{
+	public static void main(String[] args){
+		float l = 10; 
+		float b = 20;
+		
+		double perimeter = 2*(l + b);
+		System.out.println("Perimeter = " +perimeter);
+	}
+}	
