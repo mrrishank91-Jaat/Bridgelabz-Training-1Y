2f9f7526289fd4fdf5d4c3b36e34f1746fe1adbf
Rishank kumar <mrrishank91@gmail.com>
2026-02-04 22:12:36 +0530

    PowCalc

---
+++ basic program/PowCalc.java
@@ -0,0 +1,9 @@
+public class PowCalc{
+	public static void main(String[] args){
+		float b = 2;
+		float e = 5;
+		
+		double result = Math.pow(b, e);
+		System.out.println("Result =" +result);
+	}
+}	
