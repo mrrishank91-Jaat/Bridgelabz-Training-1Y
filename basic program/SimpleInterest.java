dbaf377dd79f4b735f0db3e35d0374e14bfae0af
Rishank kumar <mrrishank91@gmail.com>
2026-02-04 22:03:26 +0530

    SimpleInterst

---
+++ basic program/SimpleInterest.java
@@ -0,0 +1,10 @@
+public class SimpleInterest{
+	public static void main(String[] args){
+		float p = 1000;
+		float r = 5;
+		float t = 2;
+		
+		double si = ( p*r*t)/100;
+		System.out.println("Simple Interest =" + si);
+	}
+}
